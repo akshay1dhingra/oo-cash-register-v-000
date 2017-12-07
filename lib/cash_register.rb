7b093f257413831c2ed07b5ca9ba527d455b4bc6
Akshay Dhingra <akshay1dhingra@gmail.com>
2017-12-07 01:39:29 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
   end
 
   def add_item(title, price, quantity=1)
-    binding.pry
+    #binding.pry
     self.total = self.total + price * quantity
     quantity.times {self.items << title}
   end
