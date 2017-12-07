5219dcb5b60b8dcb5bbff234956b83a1a1ec4544
Akshay Dhingra <akshay1dhingra@gmail.com>
2017-12-07 01:43:16 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
 
   def add_item(title, price, quantity=1)
     #binding.pry
-    (self.total = self.total + price * quantity) == self.last_transaction
+    self.total = self.total + price * quantity
     quantity.times {self.items << title}
   end
 
